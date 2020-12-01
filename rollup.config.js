308268469b5fb8bdb5a61031ec38e93293fbf340
zpengg <zpengg@yeah.net>
2020-12-01 11:29:46 +0800

    fix:build

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,11 @@ import {terser} from 'rollup-plugin-terser';
 module.exports = {
   input: 'index.js',
   output:[ {
-    file: 'dist/wiki.js',
+    file: 'dist/docsify-wikilink.js',
     format: 'cjs'
   },
   {
-    file: 'dist/wiki.min.js',
+    file: 'dist/docsify-wikilink.min.js',
     format: 'iife',
     name: 'version',
     plugins: [terser()]
